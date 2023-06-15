94db030f87341b8b81f87affb223b4f5cdb7cba0
Guadalupe Mendoza <mendoza91guada@gmail.com>
2023-06-15 17:55:42 -0300

    más info

--- personaje.cs
+++ personaje.cs
@@ -2,17 +2,27 @@ namespace EspacioPersonaje
 {
     public class Personaje
         {
-            public string nombre;
-
-            public string apodo;
-
-            public int edad;
-
+            //DATOS
             enum tipo{
                 luchador= 0,
                 ciudadano= 1,
                 guardia=2,
             }
+
+            string nombre;
+            string apodo;
+            DateTime fechaNac;
+            int edad;
+
+            //CARACTERISTICAS
+            int velocidad;
+            int destreza;
+            int fuerza;
+            int nivel;
+            int armadura;
+            int salud = 100;
+
+            
         } 
 
 }
