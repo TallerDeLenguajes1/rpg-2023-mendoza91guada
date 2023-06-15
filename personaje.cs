bf8c81dc31d092ce98cb6532f38934cb1dd09552
Guadalupe Mendoza <mendoza91guada@gmail.com>
2023-06-15 17:49:06 -0300

    Segundo commit

---
+++ personaje.cs
@@ -0,0 +1,18 @@
+namespace EspacioPersonaje
+{
+    public class Personaje
+        {
+            public string nombre;
+
+            public string apodo;
+
+            public int edad;
+
+            enum tipo{
+                luchador= 0,
+                ciudadano= 1,
+                guardia=2,
+            }
+        } 
+
+}
