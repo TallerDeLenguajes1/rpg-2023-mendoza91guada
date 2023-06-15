bcb58966c2f73690656a4d827adadba6ef8c0bdd
Guadalupe Mendoza <mendoza91guada@gmail.com>
2023-06-15 18:26:14 -0300

    Haciendo pruebas con personaje 1

--- Program.cs
+++ Program.cs
@@ -7,3 +7,7 @@ Personaje personaje1 = new Personaje();
 personaje1.nombre= "William";
 
 Console.WriteLine("El nombre es "+ personaje1.nombre);
+
+personaje1.edad=33;
+
+Console.WriteLine(personaje1.nombre + " tiene " + personaje1.edad + " años");
